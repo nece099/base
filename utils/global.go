11ae9e6f5e04f2207cb1114d4a1fb3f043d833f6
dev <dev@9.com>
2021-03-08 19:30:09 +0800

    update

--- utils/global.go
+++ utils/global.go
@@ -18,16 +18,6 @@ import (
 	. "github.com/nece099/base/logger"
 )
 
-const (
-	TIME_FORMAT_WITH_MS         = "2006-01-02 15:04:05.000"
-	TIME_FORMAT                 = "2006-01-02 15:04:05"
-	TIME_FORMAT_COMPACT         = "20060102150405"
-	TIME_FORMAT_WITH_MS_COMPACT = "20060102150405.000"
-	DATE_FORMAT                 = "2006-01-02"
-	DATE_FORMAT_COMPACT         = "20060102"
-	MONTH_FORMAT                = "2006-01"
-)
-
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func NowDateStr() string {
--- utils/time_utils.go
+++ utils/time_utils.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+const (
+	TIME_FORMAT_WITH_MS         = "2006-01-02 15:04:05.000"
+	TIME_FORMAT                 = "2006-01-02 15:04:05"
+	TIME_FORMAT_COMPACT         = "20060102150405"
+	TIME_FORMAT_WITH_MS_COMPACT = "20060102150405.000"
+	DATE_FORMAT                 = "2006-01-02"
+	DATE_FORMAT_COMPACT         = "20060102"
+	MONTH_FORMAT                = "2006-01"
+)
+
 // Duration be used toml unmarshal string time, like 1s, 500ms.
 type Duration time.Duration
 
@@ -24,4 +34,8 @@ func NowFormatYMDHMS() string {
 func DateStr(t time.Time) string {
 	ds := fmt.Sprintf("%04d-%02d-%02d", t.Year(), t.Month(), t.Day())
 	return ds
+}
+
+func DateParse(dstr string) (time.Time, error) {
+	return time.Parse(DATE_FORMAT, dstr)
 }
